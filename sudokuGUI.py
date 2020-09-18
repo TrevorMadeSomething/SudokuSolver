662d959e8d371b5193ef17a75305187b544fda91
TrevorMadeSomething <54298052+TrevorMadeSomething@users.noreply.github.com>
2020-09-18 19:36:42 -0400

    modified table to be editable

--- sudokuGUI.py
+++ sudokuGUI.py
@@ -1,10 +1,27 @@
 import dash
+from dash.dependencies import Input, Output
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
 
-app = dash.Dash()
+app = dash.Dash(__name__)
 
-app.layout = html.Div('Hello World!')
+params = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+
+app.layout = html.Div([
+    dash_table.DataTable(id='table-editable',
+                         columns=([{
+                             'id': p,
+                             'name': p
+                         } for p in params]),
+                         data=[
+                             dict(Model=i, **{param: 0
+                                              for param in params})
+                             for i in range(0, 9)
+                         ],
+                         editable=True),
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
